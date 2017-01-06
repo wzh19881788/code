4b67d9b9eda2ae2c80ca0f8c2ba364cb4579442d
wzh <178866725@qq.com>
2017-01-06 03:33:19 -0800

    vector work

--- vec/vec.c
+++ vec/vec.c
@@ -49,10 +49,86 @@ int length(node *head)
 	return num;
 }
 
+bool excp(node* head, int pos)
+{
+	
+	if (pos < 0)  printf("not correct num.\n");
+
+	if (head == NULL)
+	{
+		printf("not enough link.\n");	
+		return false;
+	}
+	return true;
+}
+
+node * search_node(node *head, int pos);
+
+//
+node *insert(node *head, int pos, int data)
+{
+	node *p = head, *q;
+	
+	node *item = NULL;
+	item = (node *)malloc(sizeof(node));
+	item->data = data;
+
+	if (pos == 0)
+	{
+	     head->next = item;
+	}
+
+	q = search_node(head, pos);
+	
+	item->next = q->next;
+	q->next = item;
+
+	return p;
+}
+
+
+node *delete_node(node *head, int pos)
+{
+	node *item, *p = head;
+
+	if (head->next == NULL)
+	    return NULL;
+	
+	p = search_node(p, pos-1);
+	if (p != NULL && p->next != NULL)
+	{
+		item = p->next;
+		p->next = item->next;
+		free(item);
+	}
+	return head;
+}
+
+node * search_node(node *head, int pos)
+{
+	node *p = head;
+	if (pos < 0)  printf("not correct num.\n");
+
+	if (p == NULL)
+	{
+		printf("not enough link.\n");	
+		return NULL;
+	}
+
+	while(pos--)
+	{
+		if ((p = p->next) == NULL)
+		break;
+	}
+
+	return p;
+}
+
 int main()
 {
-	node *p = create();
+	node *p = create(), *item;
 	node *q = p;
+
 	if (p->next == NULL)
 	  printf(" %d", p->data);
 
@@ -61,8 +137,29 @@ int main()
 		printf("%d,", q->data);
 		q = q->next;	
 	}
+	printf("\n");
+	
+	q = insert(p, 6, 666);
+	while(q)
+	{
+		printf("%d,", q->data);
+		q = q->next;	
+	}
+	printf("\n");
+
+	q = delete_node(p, 4);
+	while(q)
+	{
+		printf("%d,", q->data);
+		q = q->next;	
+	}
+	printf("\n");
+
+
 
 	printf("length is :%d\n", length(p));
+
+	printf("2nd data is :%d", search_node(p, 3)->data);
 }
 
 
